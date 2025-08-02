4ecf7a5ca53676ac6156644b8bbdade00d8a7e65
19951680900 <2604211051@qq.com>
2025-08-02 15:24:53 +0800

    add home-sidebar part

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,13 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <Image
-        src="/logo.svg"
-        width={50}
-        height={50}
-        alt="Logo"
-      />
-      <p className="text-xl font-semibold tracking-tight">New Tube2</p>
+      this is main
     </div>
   );
 }
