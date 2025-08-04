523b8b0e268c894a76263e32269a638cef802ed5
19951680900 <2604211051@qq.com>
2025-08-04 14:45:53 +0800

    add sign-in sign-up parts

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -13,16 +13,24 @@ import {
 
 export const AuthButton = () => {
   return <>
-    <Button
-      variant="outline"
-      className="
-        px-4 py-2 
-        text-sm font-medium text-blue-600 
-        hover:text-blue-500 hover:cursor-pointer 
-        border-blue-500/20 rounded-full shadow-none"
-    >
-      <UserCircleIcon/>
-      Sign in
-    </Button>
+    <SignedIn>
+      <UserButton>
+      </UserButton>
+    </SignedIn>
+    <SignedOut>
+      <SignInButton mode="modal">
+        <Button
+          variant="outline"
+          className="
+            px-4 py-2 
+            text-sm font-medium text-blue-600 
+            hover:text-blue-500 hover:cursor-pointer 
+            border-blue-500/20 rounded-full shadow-none"
+        >
+          <UserCircleIcon/>
+          Sign in
+        </Button>
+      </SignInButton>
+    </SignedOut>
   </>
 }
