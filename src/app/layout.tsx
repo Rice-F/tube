2a3b0dafe16024490107cf42f9323943d2f63984
19951680900 <2604211051@qq.com>
2025-08-13 15:19:05 +0800

    修复layout多写了children

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,6 @@ export default function RootLayout({
             <TRPCProvider>
               {children}
             </TRPCProvider>
-            {children}
           </body>
         </html>
     </ClerkProvider>
