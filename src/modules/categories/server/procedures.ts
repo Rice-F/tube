db150debee018f1ef6f7248cd277cb4559eda0d8
19951680900 <2604211051@qq.com>
2025-08-13 16:57:56 +0800

    create categories trpc router & add to trpc app

---
+++ src/modules/categories/server/procedures.ts
@@ -0,0 +1,12 @@
+import { db } from "@/db";
+import { categories } from "@/db/schema";
+import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+
+export const categoriesRouter = createTRPCRouter({
+  getAll: baseProcedure.query(async () => {
+    const data = await db
+      .select()
+      .from(categories)
+    return data;
+  })
+})
