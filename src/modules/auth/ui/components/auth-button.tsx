095bd91f64b959de51fd496dca85738104e2d7e3
19951680900 <2604211051@qq.com>
2025-08-20 10:38:04 +0800

    add studio in clerk user dropdown

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -12,25 +12,36 @@ import {
 } from '@clerk/nextjs'
 
 export const AuthButton = () => {
-  return <>
-    <SignedIn>
-      <UserButton>
-      </UserButton>
-    </SignedIn>
-    <SignedOut>
-      <SignInButton mode="modal">
-        <Button
-          variant="outline"
-          className="
-            px-4 py-2 
-            text-sm font-medium text-blue-600 
-            hover:text-blue-500 hover:cursor-pointer 
-            border-blue-500/20 rounded-full shadow-none"
-        >
-          <UserCircleIcon/>
-          Sign in
-        </Button>
-      </SignInButton>
-    </SignedOut>
-  </>
+  return (
+    // 空标签表示不渲染任何DOM元素
+    // 在返回多个元素时，不用必须被div包裹，不影响布局、样式
+    <>
+      <SignedIn>
+        <UserButton>
+          <UserButton.MenuItems>
+            <UserButton.Link
+              label="Studio"
+              href="/studio"
+              labelIcon={<ClapperboardIcon className="size-4" />}
+            />
+          </UserButton.MenuItems>
+        </UserButton>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button
+            variant="outline"
+            className="
+              px-4 py-2 
+              text-sm font-medium text-blue-600 
+              hover:text-blue-500 hover:cursor-pointer 
+              border-blue-500/20 rounded-full shadow-none"
+          >
+            <UserCircleIcon/>
+            Sign in
+          </Button>
+        </SignInButton>
+      </SignedOut>
+    </>
+  )
 }
