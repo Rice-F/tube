30e41bd9a10a91bacb63f091f82375a860274da1
19951680900 <2604211051@qq.com>
2025-08-21 15:27:01 +0800

    add part of the studio sidebar

--- src/modules/studio/ui/components/studio-sidebar.tsx
+++ src/modules/studio/ui/components/studio-sidebar.tsx
@@ -1,5 +1,50 @@
+'use client'
+
+import { 
+  Sidebar, 
+  SidebarContent, 
+  SidebarGroup, 
+  SidebarMenu, 
+  SidebarMenuButton, 
+  SidebarMenuItem 
+} from "@/components/ui/sidebar"
+import { Separator } from "@/components/ui/separator";
+
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+
+import { LogOutIcon, VideoIcon } from "lucide-react";
+import path from "path";
+
 export const StudioSidebar = () => {
+  const pathname = usePathname();
+  console.log('路径', pathname);
+
   return (
-    <div>side</div>
+    <Sidebar className="pt-16 z-40" collapsible="icon">
+      <SidebarContent className="bg-background">
+        <SidebarGroup>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton isActive={pathname === '/studio'} tooltip="Content" asChild>
+                <Link href="/studio">
+                  <VideoIcon className="size-5" />
+                  <span className="text-sm">Content</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+            <Separator />
+            <SidebarMenuItem>
+              <SidebarMenuButton tooltip="Exit Studio" asChild>
+                <Link href="/">
+                  <LogOutIcon className="size-5" />
+                  <span className="text-sm">Exit Studio</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
   )
 }
