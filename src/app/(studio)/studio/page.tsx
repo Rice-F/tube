a5b1bca9298805e3118407e6ca4b04f886192462
19951680900 <2604211051@qq.com>
2025-08-23 10:12:28 +0800

    add studio video-section

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,6 +1,18 @@
+import { HydrateClient, trpc } from '@/trpc/server';
+
+import { DEFAULT_LIMIT } from '@/constants';
+
+import { StudioView } from '@/modules/studio/ui/views/studio-view';
+
 const Page = async () => {
+  void trpc.studio.getAll.prefetchInfinite({
+    limit: DEFAULT_LIMIT
+  });
+
   return (
-    <div>studio page</div>
+    <HydrateClient>
+      <StudioView />
+    </HydrateClient>
   )
 }
 
---
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { trpc } from '@/trpc/client'
+
+import { Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary';
+
+import { Skeleton } from '@/components/ui/skeleton';
+
+import { DEFAULT_LIMIT } from '@/constants';
+
+export const VideosSection = () => {
+  return (
+    <Suspense fallback={<VideosSkeleton />}>
+      <ErrorBoundary fallback={<p>Error...</p>}>
+        <VideosSectionSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  )
+}
+
+export const VideosSkeleton = () => {
+  return (
+    <Skeleton></Skeleton>
+  )
+}
+
+export const VideosSectionSuspense = () => {
+  const [videos, query] = trpc.studio.getAll.useSuspenseInfiniteQuery(
+    { limit: DEFAULT_LIMIT },
+    { getNextPageParam: lastPage => lastPage.nextCursor }
+  )
+
+  return (
+    <div>suspense</div>
+  )
+}
