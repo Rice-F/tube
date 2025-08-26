b34ffa23bb21936ea96a36fd05e462abd0a4c6e9
19951680900 <2604211051@qq.com>
2025-08-26 09:00:44 +0800

    add studio-upload-modal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 import { TRPCProvider } from "@/trpc/client";
 
+import { Toaster } from "@/components/ui/sonner";
+
 const inter = Inter({ subsets: ["latin"] });
  
 export const metadata: Metadata = {
@@ -24,6 +26,7 @@ export default function RootLayout({
         <html lang="en">
           <body className={ inter.className }>
             <TRPCProvider>
+              <Toaster />
               {children}
             </TRPCProvider>
           </body>
--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,9 +1,37 @@
 "use client";
 
+import { trpc } from "@/trpc/client";
+
+import { Button } from '@/components/ui/button';
+
+import { Loader2Icon, PlusIcon } from 'lucide-react';
+
+import { toast } from 'sonner'
+
 export const StudioUploadModal = () => {
+  const utils = trpc.useUtils()
+
+  const createVideo = trpc.videos.create.useMutation({
+    onSuccess: () => {
+      toast.success('Video created')
+      utils.studio.getAll.invalidate() // 使查询缓存失效，强制获取最新数据
+    },
+    onError: (err) => {
+      toast.error(err.message)
+    }
+  })
   return (
     <>
-      modal
+      {/* create.isPending 是否请求中，是否正在mutation */}
+      <Button
+        variant="secondary"
+        onClick={() => createVideo.mutate()} // 触发mutation，提交请求
+        disabled={createVideo.isPending}
+        className="cursor-pointer"
+      >
+        {createVideo.isPending ? <Loader2Icon className='animate-spin' /> : <PlusIcon />}
+        Create
+      </Button>
     </>
   )
 }
