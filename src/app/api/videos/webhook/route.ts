987e63e38f5b91bb637537b329edfae0241b2a3b
19951680900 <2604211051@qq.com>
2025-08-29 17:39:14 +0800

    add mux events webhook

--- src/app/api/videos/webhook/route.ts
+++ src/app/api/videos/webhook/route.ts
@@ -61,6 +61,75 @@ export const POST = async (request: Request) => {
         .where(eq(videos.muxUploadId, payloadData.upload_id)) // 与数据库数据做匹配
       break;
     }
+
+    // 已准备好播放
+    case "video.asset.ready": {
+      const payloadData = payload.data as VideoAssetCreatedWebhookEvent["data"]
+      const playbackId = payloadData.playback_ids?.[0]?.id
+
+      if(!payloadData.upload_id || !playbackId) {
+        // return logger.error("Missing upload Id or playback ID")
+        throw new Response("Missing upload Id or playback ID", { status: 400 })
+      }
+
+      const tempThumbnailUrl = `https://image.mux.com/${playbackId}/thumbnail.jpg`
+      const tempPreviewUrl = `https://image.mux.com/${playbackId}/animated.gif`
+      const duration = payloadData.duration ? Math.round(payloadData.duration * 1000) : 0;
+
+      await db
+        .update(videos)
+        .set({
+          muxStatus: payloadData.status,
+          muxPlaybackId: playbackId,
+          muxAssetId: payloadData.id,
+          thumbnailUrl: tempThumbnailUrl,
+          previewUrl: tempPreviewUrl,
+          duration
+        })
+        .where(eq(videos.muxUploadId, payloadData.upload_id))
+      break;
+    }
+
+    // error
+    case 'video.asset.errored': {
+      const payloadData = payload.data as VideoAssetCreatedWebhookEvent["data"]
+
+      if(!payloadData.upload_id) return new Response("Missing upload Id", { status: 400 })
+
+      await db
+        .update(videos)
+        .set({ muxStatus: payloadData.status })
+        .where(eq(videos.muxUploadId, payloadData.upload_id))
+      break;
+    }
+
+    // 已删除
+    case 'video.asset.deleted': {
+      const payloadData = payload.data as VideoAssetDeletedWebhookEvent['data']
+
+      if(!payloadData.upload_id) return new Response("Missing upload Id", { status: 400 })
+
+      await db
+        .delete(videos)
+        .where(eq(videos.muxUploadId, payloadData.upload_id))
+      break;
+    }
+
+    // 字幕文本轨道就绪
+    case "video.asset.track.ready": {
+      const payloadData = payload.data as VideoAssetTrackReadyWebhookEvent['data'] & { asset_id: string }
+
+      if(!payloadData.asset_id) return new Response('Missing asset ID', {status: 400})
+
+      await db
+        .update(videos)
+        .set({
+          muxTrackId: payloadData.id,
+          muxStatus: payloadData.status
+        })
+        .where(eq(videos.muxAssetId, payloadData.asset_id))
+      break;
+    }
   }
 
   return new Response('Webhook received', { status: 200 })
