d3848b58f0ae4f456aeced551f15219355978c27
19951680900 <2604211051@qq.com>
2025-08-29 17:36:26 +0800

    add image remote config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      // 允许从 https://image.mux.com/... 加载图片
+      {
+        protocol: 'https',
+        hostname: 'image.mux.com'
+      },
+      // uploadthing 的图片
+      {
+        protocol: 'https',
+        hostname: 'szxu79mai4.ufs.sh'
+      }
+    ]
+  } 
 };
 
 export default nextConfig;
