b3a9e21ec7a7c2172ba5d0cd6b3d79267fb1398d
19951680900 <2604211051@qq.com>
2025-08-29 17:36:56 +0800

    add utils function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// 将以毫秒为单位的duration格式化为"mm:ss"的字符串格式
+// padStart() 补0
+export const formatDuration = (duration: number) => {
+  const seconds = Math.floor((duration % 60000) / 1000)
+  const minutes = Math.floor(duration / 60000)
+  return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+}
+
+export const snakeCaseToTitle = (str: string) => {
+  return str.replace(/_/g, '').replace(/\b\w/g, char => char.toUpperCase())
+}
