22e5787c612663627cf285c2a892b3c16526b116
19951680900 <2604211051@qq.com>
2025-08-31 17:03:16 +0800

    add videos delete trpc route

--- src/modules/videos/server/procedure.ts
+++ src/modules/videos/server/procedure.ts
@@ -12,45 +12,46 @@ import { mux } from '@/lib/mux'
 
 export const videosRouter = createTRPCRouter({
   // mutation对应对数据库的增删改，表示会修改数据库数据
-  create: protectedProcedure.mutation(async ({ ctx }) => {
-    const { id: userId } = ctx.user
+  create: protectedProcedure
+    .mutation(async ({ ctx }) => {
+      const { id: userId } = ctx.user
 
-    // mux-node-sdk提供的：创建一个新的 Direct Upload 接口
-    // Direct Upload 表示前端直接将视频文件上传到 Mux，不需要经过自己的服务器
-    const upload = await mux.video.uploads.create({
-      new_asset_settings: {
-        passthrough: userId, // 传递用户ID到Mux
-        playback_policy: ['public'], // 设置播放策略为公开
-        // mp4_support: "standard", // 设置MP4支持为标准
-        input: [
-          {
-            generated_subtitles: [
-              {
-                language_code: 'en',
-                name: 'English'
-              }
-            ]
-          }
-        ]
-      },
-      cors_origin: '*', // 允许所有CORS来源
-    })
-
-    const [video] = await db
-      .insert(videos)
-      .values({
-        userId,
-        title: 'Untitled',
-        muxStatus: "waiting",
-        muxUploadId: upload.id
+      // mux-node-sdk提供的：创建一个新的 Direct Upload 接口
+      // Direct Upload 表示前端直接将视频文件上传到 Mux，不需要经过自己的服务器
+      const upload = await mux.video.uploads.create({
+        new_asset_settings: {
+          passthrough: userId, // 传递用户ID到Mux
+          playback_policy: ['public'], // 设置播放策略为公开
+          // mp4_support: "standard", // 设置MP4支持为标准
+          input: [
+            {
+              generated_subtitles: [
+                {
+                  language_code: 'en',
+                  name: 'English'
+                }
+              ]
+            }
+          ]
+        },
+        cors_origin: '*', // 允许所有CORS来源
       })
-      .returning()
 
-    return {
-      video,
-      uploadUrl: upload.url // 返回上传URL给前端
-    }
-  }),
+      const [video] = await db
+        .insert(videos)
+        .values({
+          userId,
+          title: 'Untitled',
+          muxStatus: "waiting",
+          muxUploadId: upload.id
+        })
+        .returning()
+
+      return {
+        video,
+        uploadUrl: upload.url // 返回上传URL给前端
+      }
+    }),
   update: protectedProcedure
     .input(videosUpdateSchema) // 使用videosUpdateSchema作为输入验证规则
     .mutation(async ({ ctx, input }) => {
@@ -76,5 +77,22 @@ export const videosRouter = createTRPCRouter({
       if(!updatedVideo) throw new TRPCError({ code: 'NOT_FOUND' })
 
       return updatedVideo
+    }),
+  delete: protectedProcedure
+    .input(z.object({videoId: z.uuid()}))
+    .mutation(async ({ ctx, input }) => {
+      const {id: userId} = ctx.user
+
+      const [deletedVideo] = await db
+        .delete(videos)
+        .where(and(
+          eq(videos.userId, userId),
+          eq(videos.id, input.videoId)
+        ))
+        .returning()
+
+      if(!deletedVideo) throw new TRPCError({ code: 'NOT_FOUND' })
+
+      return deletedVideo
     })
 })
