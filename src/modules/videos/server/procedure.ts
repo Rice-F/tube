87b76089a56f32b650b6302a4df22aedd816a4a2
19951680900 <2604211051@qq.com>
2025-09-12 11:11:51 +0800

    add generate thumbnail trpc route

--- src/modules/videos/server/procedure.ts
+++ src/modules/videos/server/procedure.ts
@@ -148,9 +148,16 @@ export const videosRouter = createTRPCRouter({
       return updatedVideo
     }),
   generateThumbnail: protectedProcedure
-    .input(z.object({ videoId: z.uuid() }))
+    .input(z.object({ id: z.uuid(), prompt: z.string().min(10) }))
     .mutation(async ({ ctx, input }) => {
       const { id: userId } = ctx.user
+
+      const { workflowRunId } = await workflow.trigger({
+        url: `${process.env.UPSTASH_WORKFLOW_URL}/api/videos/workflows/thumbnail`,
+        body: { userId, videoId: input.id, prompt: input.prompt }, 
+      }) 
+
+      return workflowRunId
     }),
   generateTitle: protectedProcedure
     .input(z.object({ videoId: z.uuid() }))
