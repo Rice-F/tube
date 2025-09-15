fa399942201da3dbda8003e47976ad72a296c613
19951680900 <2604211051@qq.com>
2025-09-15 10:52:45 +0800

    add video page approuter

---
+++ src/app/(home)/videos/[videoId]/page.tsx
@@ -0,0 +1,21 @@
+import { HydrateClient, trpc } from '@/trpc/server';
+
+import { VideoView } from '@/modules/videos/ui/views/video-view';
+
+interface VideoIdPageProps {
+  params: Promise<{videoId: string}>;
+}
+
+const Page = async ({ params }: VideoIdPageProps) => {
+  const { videoId } = await params;
+
+  void trpc.videos.getOne.prefetch({ videoId });
+
+  return (
+    <HydrateClient>
+      <VideoView videoId = {videoId} />
+    </HydrateClient>
+  )
+}
+
+export default Page
