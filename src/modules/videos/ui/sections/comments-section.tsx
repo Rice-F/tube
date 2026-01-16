f3fb6e947d349cae050cba812b27330831f117ab
19951680900 <2604211051@qq.com>
2026-01-16 21:39:54 +0800

    comment-section

--- src/modules/videos/ui/sections/comments-section.tsx
+++ src/modules/videos/ui/sections/comments-section.tsx
@@ -1,5 +1,36 @@
-export const CommentsSection = () => {
+'use client';
+
+import { trpc } from '@/trpc/client';
+
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+
+import { CommentForm } from '@/modules/comments/ui/components/comment-form';
+
+interface CommentsSectionProps {
+  videoId: string;
+}
+
+export const CommentsSection = ({ videoId }: CommentsSectionProps) => {
   return (
-    <div>comments</div>
+    <Suspense fallback={<div>Loading comments...</div>}>
+      <ErrorBoundary fallback={<p>Error</p>}>
+        <CommentsSectionSuspense videoId={videoId} />
+      </ErrorBoundary>
+    </Suspense>
   )
 }
+
+const CommentsSectionSuspense = ({ videoId }: CommentsSectionProps) => {
+  const [comments] = trpc.comments.getAll.useSuspenseQuery({ videoId });
+
+  return (
+    <div className='mt-6'>
+      <div className='flex flex-col gap-6'>
+        <h1>0 Comments</h1>
+        <CommentForm videoId={ videoId } />
+      </div>
+      {JSON.stringify(comments)}
+    </div>
+  )
+} 
